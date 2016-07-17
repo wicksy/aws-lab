6d9b49894e7f10705281cdb46449b15bb7fc090e
Martin Wicks <wicksy@wicksy.com>
2016-07-17 14:25:10 +0100

    Update env vars

--- terraform/personal/envs.sh
+++ terraform/personal/envs.sh
@@ -6,7 +6,7 @@ export AWS_SECRET_KEY="insertsecrethere"
 export TF_VAR_aws_account_number="insertawsaccounthere"
 
 export TF_VAR_aws_account=wicksy
-export TF_VAR_stack=IAMPoliciesAndUsers
+export TF_VAR_stack=S3IAMPoliciesAndUsers
 
 mkdir -p ./${TF_VAR_aws_account}/${TF_VAR_stack} 2>/dev/null
 set +x
